ce24ce4b3fae067f794d502e322a018e6d62fa1c
fienalf02 <fifienalfinie@gmail.com>
2019-07-16 15:04:08 +0700

    menambahkan crud kelas

---
+++ app/Http/Controllers/KelasController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Kelas;
+
+class KelasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $kelas = Kelas::all();
+        return $kelas;   
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $kelas =  new Kelas;
+        $kelas->id_jurusan = $request->id_jurusan;
+        $kelas->kelas = $request->kelas;
+        $kelas->save();
+
+        return "Data berhasil dimasukkan";
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $id_jurusan = $request->id_jurusan;
+        $kelas = $request->kelas;
+
+        $kelass = Kelas::find($id);
+        $kelass->id_jurusan = $id_jurusan;
+        $kelass->kelas = $kelas;
+        $kelass->save();
+
+        return "Data berhasil diUpdate";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $kelas = Kelas::find($id);
+        $kelas->delete();
+
+        return "Data berhasil dihapus";
+    }
+}
