4a5104fbe90360bbdde1d057715f58c76a797009
fienalf02 <fifienalfinie@gmail.com>
2019-07-25 14:22:45 +0700

    nambahin index buat tampilan dashboard

---
+++ resources/views/index.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="au theme template">
+    <meta name="author" content="Hau Nguyen">
+    <meta name="keywords" content="au theme template">
+
+    <!-- Title Page-->
+    <title>Dashboard</title>
+
+    @include('include.link')
+
+</head>
+
+<body class="animsition">
+    <div class="page-wrapper">
+        
+        @include('include.header')
+
+            <!-- MAIN CONTENT-->
+            <div class="main-content">
+                <div class="section__content section__content--p30">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="overview-wrap">
+                                    
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- END MAIN CONTENT-->
+            <!-- END PAGE CONTAINER-->
+        </div>
+
+    </div>
+
+    @include('include.script')
+
+</body>
+
+</html>
+<!-- end document-->
