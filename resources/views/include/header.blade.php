c1ec76d2976b141831224de4c30efe62c292784c
fienalf02 <fifienalfinie@gmail.com>
2019-07-25 14:21:49 +0700

    nambahin folder buat diinclude

---
+++ resources/views/include/header.blade.php
@@ -0,0 +1,203 @@
+<!-- HEADER MOBILE-->
+<header class="header-mobile d-block d-lg-none">
+            <div class="header-mobile__bar">
+                <div class="container-fluid">
+                    <div class="header-mobile-inner">
+                        <p  >
+                            <img src="assets/images/icon/lhp.png" alt="Admin..."  style="height: 70px"/>
+                        </p>
+                        <button class="hamburger hamburger--slider" type="button">
+                            <span class="hamburger-box">
+                                <span class="hamburger-inner"></span>
+                            </span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <nav class="navbar-mobile">
+                <div class="container-fluid">
+                    <ul class="navbar-mobile__list list-unstyled">
+                                
+                             <li >
+                                <a  class="example_e" href="/home" target="self" rel="nofollow noopener">
+                                <i class="fas fa-tachometer-alt  fa-lg"></i>Dashboard</a>
+                                
+                            <li>
+                                <a class="example_e" href="/tabeljurusan" target="self" rel="nofollow noopener">
+                                <i class="fa fa-archive fa-lg"></i>Jurusan</a>
+                            </li>
+                            <li>
+                                <a class="example_e" href="/tabelkelas" target="self" rel="nofollow noopener">
+                                <i class="fas fa-table fa-lg"></i>Kelas</a>
+                            </li>
+                            <li>
+                                <a class="example_e" href="/tabelguru" target="self" rel="nofollow noopener">
+                                <i class="fas fa-chalkboard-teacher fa-lg "></i>Guru </a>
+                            </li>
+
+                            <li>
+                                <a class="example_e" href="/tabelsiswa" target="self" rel="nofollow noopener">
+                                <i class="fas fa-user fa-lg"></i>Siswa </a>
+                            </li>
+
+                            <ul class="list-unstyled navbar__sub-list js-sub-list">
+                        
+                            </ul>
+                            </li>
+                            <li>
+                                <a class="example_e" href="/tabelnilai" target="self" rel="nofollow noopener">
+                                <i class="fas fa-file fa-lg"></i>Nilai </a>
+                            </li>
+                            <li>
+                                <a class="example_e" href="/tabeldetailnilai" target="self" rel="nofollow noopener">
+                                <i class="fas fa-copy fa-lg"></i>Detail Nilai </a>
+                            </li>
+    
+                            <li>
+                                <a class="example_e" href="/tabelmapel" target="self" rel="nofollow noopener">
+                                <i class="fas fa-book fa-lg"></i>Mata Pelajaran</a>
+                            </li>
+    
+                            <li>
+                                <a class="example_e" href="tabeljadwal" target="self" rel="nofollow noopener">
+                                <i class="fas fa-calendar-alt fa-lg"></i>Jadwal</a>
+                            </li>
+    
+                                    
+                    </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <!-- END HEADER MOBILE-->
+
+        <!-- MENU SIDEBAR-->
+        <aside class="menu-sidebar d-none d-lg-block">
+            <div class="logo">
+                <a href="#">
+                        <a class="logoo" href="index.html">
+                                <img src="assets/images/icon/Ld.png" alt="Admin" />
+                            
+
+                        <!-- <i class="fas fa-user-circle fa-3x " style="color:darkcyan">ADMIN</i> -->
+                    <!-- <img src="images/icon/.jpg " style="size: 40" alt="LOGO" /> -->
+                </a>
+            </div>
+            <div class="menu-sidebar__content js-scrollbar1">
+                <nav class="navbar-sidebar bg-whte md-0  pr-1 pt-0 mt-4 ml-0">
+                    <ul class="list-unstyled navbar__list">
+                           
+                                <li > <hr>
+                                    <a class="example_a" href="/home" target="self" rel="nofollow noopener">
+                                    <i class="fas fa-tachometer-alt  fa-lg ml-3"></i>Dashboard</a>
+                            
+                                <li><tr>
+                                    <a class="example_a" href="/tabeljurusan" target="self" rel="nofollow noopener">
+                                    <i class="fa fa-archive fa-lg ml-3"></i>Jurusan</a>
+                                </li>
+                                <li>
+                                    <a class="example_a" href="/tabelkelas" target="self" rel="nofollow noopener" href="kelas.php">
+                                    <i class="fas fa-table fa-lg ml-3"></i>Kelas</a>
+                                </li>
+                                <li>
+                                    <a class="example_a" href="/tabelguru" target="self" rel="nofollow noopener">
+                                    <i class="fas fa-user-circle fa-lg ml-3"></i>Guru </a>
+                                </li>
+                               
+                                <li>
+                                    <a class="example_a" href="/tabelsiswa" target="self" rel="nofollow noopener">
+                                    <i class="fas fa-user fa-lg ml-3"></i>Siswa </a>
+                                 </li>
+        
+                            <ul class="list-unstyled navbar__sub-list js-sub-list">
+                               
+                                </ul></li>
+                                <li>
+                                    <a class="example_a" href="/tabelnilai" target="self" rel="nofollow noopener">
+                                    <i class="fas fa-file fa-lg ml-3"></i>Nilai </a>
+                                </li>
+                                <li>
+                                    <a class="example_a" href="/tabeldetailnilai" target="self" rel="nofollow noopener">
+                                    <i class="fas fa-copy fa-lg ml-3"></i>Detail Nilai </a>
+                                 </li>
+
+                                <li>
+                                    <a class="example_a" href="/tabelmapel" target="self" rel="nofollow noopener">
+                                    <i class="fas fa-book fa-lg ml-3"></i>Mata Pelajaran</a>
+                                </li>
+
+                                <li>
+                                    <a class="example_a" href="/tabeljadwal" target="self" rel="nofollow noopener">
+                                    <i class="fas fa-calendar-alt fa-lg ml-3"></i>Jadwal</a>
+                                </li>  
+                                </li><hr>
+                            </ul> 
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </aside>
+        <!-- END MENU SIDEBAR-->
+
+         <!-- PAGE CONTAINER-->
+        <div class="page-container">
+            <!-- HEADER DESKTOP-->
+            <header class="header-desktop">
+                <div class="section__content section__content--p30">
+                    <div class="container-fluid">
+                        <div class="header-wrap">
+                            <div class="judul">Dashboard</div>
+                            <div class="btnn">
+                            <button ><p style="color: rgb(245, 115, 63)"><i class="fab fa-instagram fa-3x ml-9" ></i></p></button>
+                            <button> <p style="color: rgb(41, 73, 128) " ><i class="fas fa-envelope fa-3x ml-3"></i></p></button></div>
+                        </form>
+                        
+                           <!-- <div > 
+                               
+
+                           <button> <p style="color: rgb(107, 107, 233)"> <i class="fab fa-twitter-square fa-3x"></i></p></button>
+                          </div> -->
+                            
+                
+
+                          <div class="account-wrap">
+                               @if (Auth::guest())
+                                    <li><a href="{{ route('login') }}">Login</a></li>
+                                    <li><a href="{{ route('register') }}">Register</a></li>
+                                @else
+                                    <div class="account-item clearfix js-item-menu">
+                                        <div class="content">
+                                            <i class="fas fa-user fa-lg fa-2.5x"></i>  <a class="js-acc-btn" href="#">{{ Auth::user()->name }}</a>
+                                        </div>
+                                        <div class="account-dropdown js-dropdown">
+                                            <div class="info clearfix">
+                                                <div class="image">
+                                                    <i class="fas fa-user-circle fa-4x"></i>
+                                                </div>
+                                                <div class="content">
+                                                    <h5 class="name">
+                                                        <a href="#">{{ Auth::user()->name }}</a>
+                                                    </h5>
+                                                    <span class="email">{{ Auth::user()->email }}</span>
+                                                </div>
+                                            </div>
+                                            <div class="account-dropdown__footer">
+                                                <div class="account-dropdown__item">
+                                                    <a href="{{ route('logout') }}"
+                                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                                        <i class="zmdi zmdi-power"></i>Logout</a>
+                                                
+                                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                    {{ csrf_field() }}
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                @endif
+                        </div></div></div>
+            </header>
+            <!-- HEADER DESKTOP-->
